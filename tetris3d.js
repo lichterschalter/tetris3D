ea8caebc530f217a62fab990561d08c803a3b250
Fabian <lichterschalter@gmx.at>
2015-11-06 20:04:37 +0100

    one side of the grid back created

---
+++ tetris3d.js
@@ -0,0 +1,365 @@
+var gl;
+
+function initGL( canvas ){
+    gl = WebGLUtils.setupWebGL( canvas );
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+}
+
+
+function getShader(gl, id) {
+    var shaderScript = document.getElementById(id);
+    if (!shaderScript) {
+        return null;
+    }
+
+    var str = "";
+    var k = shaderScript.firstChild;
+    while (k) {
+        if (k.nodeType == 3) {
+            str += k.textContent;
+        }
+        k = k.nextSibling;
+    }
+
+    var shader;
+    if (shaderScript.type == "x-shader/x-fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+        return null;
+    }
+
+    gl.shaderSource(shader, str);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(gl.getShaderInfoLog(shader));
+        return null;
+    }
+
+    return shader;
+}
+
+
+var shaderProgram;
+
+function initShaders() {
+    var fragmentShader = getShader(gl, "shader-fs");
+    var vertexShader = getShader(gl, "shader-vs");
+
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Could not initialise shaders");
+    }
+
+    gl.useProgram(shaderProgram);
+
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+    shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+
+    shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+    shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+}
+
+
+var mvMatrix = mat4.create();
+var mvMatrixStack = [];
+var pMatrix = mat4.create();
+
+function mvPushMatrix() {
+    var copy = mat4.create();
+    mat4.set(mvMatrix, copy);
+    mvMatrixStack.push(copy);
+}
+
+function mvPopMatrix() {
+    if (mvMatrixStack.length == 0) {
+        throw "Invalid popMatrix!";
+    }
+    mvMatrix = mvMatrixStack.pop();
+}
+
+
+function setMatrixUniforms() {
+    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+
+
+function degToRad(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+
+var currentlyPressedKeys = {};
+
+function handleKeyDown(event) {
+    currentlyPressedKeys[event.keyCode] = true;
+
+    if (String.fromCharCode(event.keyCode) == "F") {
+        filter += 1;
+        if (filter == 3) {
+            filter = 0;
+        }
+    }
+}
+
+
+function handleKeyUp(event) {
+    currentlyPressedKeys[event.keyCode] = false;
+}
+
+
+
+var redBg;
+var greenBg;
+var blueBg;
+
+function initBuffers() {
+
+    //GRID BACK
+
+    //horizontal lines
+    gridBackHorizontalPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBackHorizontalPositionBuffer);
+    vertices = []
+      for( var y = 0.0; y > -16; --y ){
+        var x = 0.0;
+        var z = 0.0;
+        vertices = vertices.concat([
+          x,        y,        z,
+          (x + 10), y,        z,
+        ]);
+        }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gridBackHorizontalPositionBuffer.itemSize = 3;
+    gridBackHorizontalPositionBuffer.numItems = 32; //plus one for every new row, for triangle hiding
+
+    gridBackHorizontalColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBackHorizontalColorBuffer);
+    colors = []
+    for( var y = 0; y < 16; ++y ){
+        colors = colors.concat([
+          1.0, 1.0, 1.0, 1.0,
+          1.0, 1.0, 1.0, 1.0,
+          ]);
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    gridBackHorizontalColorBuffer.itemSize = 4;
+    gridBackHorizontalColorBuffer.numItems = 32;
+
+    //vertical lines
+    gridBackVerticalPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBackVerticalPositionBuffer);
+    vertices = []
+      for( var x = 0.0; x < 11; ++x ){
+        var y = 0.0;
+        var z = 0.0;
+        vertices = vertices.concat([
+          x,        y,        z,
+          x, (y - 15),        z,
+        ]);
+        }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gridBackVerticalPositionBuffer.itemSize = 3;
+    gridBackVerticalPositionBuffer.numItems = 22; //plus one for every new row, for triangle hiding
+
+    gridBackVerticalColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBackVerticalColorBuffer);
+    colors = []
+    for( var x = 0; x < 11; ++x ){
+        colors = colors.concat([
+          1.0, 1.0, 1.0, 1.0,
+          1.0, 1.0, 1.0, 1.0,
+          ]);
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    gridBackVerticalColorBuffer.itemSize = 4;
+    gridBackVerticalColorBuffer.numItems = 22;
+
+
+    //BACKGROUND
+    bgPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bgPositionBuffer);
+    vertices = [
+         100.0,  100.0,  0.0,
+        -100.0,  100.0,  0.0,
+         100.0, -100.0,  0.0,
+        -100.0, -100.0,  0.0
+        ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    bgPositionBuffer.itemSize = 3;
+    bgPositionBuffer.numItems = 4;
+
+    bgColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bgColorBuffer);
+    colors = []
+    for (var i = 0; i < 4; ++i) {
+        colors = colors.concat([redBg, greenBg, blueBg, 1.0]);
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    bgColorBuffer.itemSize = 4;
+    bgColorBuffer.numItems = 4;
+
+}
+
+
+var rotate_tetrimon = 90;
+var tetrimon_beforeRotation = rotate_tetrimon;
+var positionX_tetrimon = 5.0;
+var positionY_tetrimon = 6.5;
+var positionZ_tetrimon = -20.0;
+var gameSize = 45;
+
+function drawScene() {
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    mat4.perspective(gameSize, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+
+    mat4.identity(mvMatrix);
+
+
+    //DRAW GRID BACK
+
+    //horizontal lines
+    mvPushMatrix();
+
+    mat4.translate(mvMatrix, [0, 0, -40]);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBackHorizontalPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, gridBackHorizontalPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBackHorizontalColorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, gridBackHorizontalColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    setMatrixUniforms();
+    gl.drawArrays(gl.LINES, 0, gridBackHorizontalPositionBuffer.numItems);
+
+    mvPopMatrix();
+
+    //vertical lines
+    mvPushMatrix();
+
+    mat4.translate(mvMatrix, [0, 0, -40]);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBackVerticalPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, gridBackVerticalPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBackVerticalColorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, gridBackVerticalColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    setMatrixUniforms();
+    gl.drawArrays(gl.LINES, 0, gridBackVerticalPositionBuffer.numItems);
+
+    mvPopMatrix();
+
+
+    //DRAW BACKGROUND
+    mvPushMatrix();
+
+    mat4.translate(mvMatrix, [0, 0, -90.1]);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, bgPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, bgPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, bgColorBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, bgColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, bgPositionBuffer.numItems);
+
+    mvPopMatrix();
+
+}
+
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+var tetrimonType;
+//var tetrimonType = "one_x_four";
+function typeOfCurrentTetrimon() {
+    type = getRandomNumber(0,1);
+
+    if( type == 0){
+    //currentObject = new two_x_two();
+    tetrimonType = "two_x_two";
+    }
+
+    if( type == 1){
+    //currentObject = new one_x_four();
+    tetrimonType = "one_x_four";
+    }
+}
+
+
+function initGame() {
+    redBg = Math.random();
+    greenBg = Math.random();
+    blueBg = Math.random();
+
+    typeOfCurrentTetrimon();
+
+}
+
+
+
+var lastTime = 0;
+function animate() {
+
+  //gravity();
+
+  var timeNow = new Date().getTime();
+  if (lastTime != 0) {
+    var elapsed = timeNow - lastTime;
+
+    //rotateObject( elapsed );
+
+  }
+  lastTime = timeNow;
+
+}
+
+
+var gameOver = false;
+function tick() {
+    if( !gameOver ){
+        requestAnimFrame(tick);
+        //handleKeys();
+        drawScene();
+        animate();
+    }else{
+        window.alert("Game Over!");
+    }
+}
+
+
+function webGLStart() {
+    var canvas = document.getElementById("lesson03-canvas");
+    initGL(canvas);
+    initShaders();
+    initGame();
+    initBuffers();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+
+    tick();
+}
